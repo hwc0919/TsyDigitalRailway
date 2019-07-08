290e39cefb46a2f88e27e815981212f6db6d6049
hwc0919 <hwc14@qq.com>
2019-07-08 16:28:13 +0800

    change website description header in overview; enable website cache.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'user1418'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(days=2)
-    SEND_FILE_MAX_AGE_DEFAULT = timedelta(seconds=1)
+    # SEND_FILE_MAX_AGE_DEFAULT = timedelta(seconds=1)
 
     @staticmethod
     def init_app(app):
